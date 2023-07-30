cd9abe9ff684121ec283ddd42ae74b10f8a16a96
mohssin <mohssinassalh@gmail.com>
2023-07-30 14:57:41 +0100

    commit to main

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+// import Login from './components/Login/Login';
+import Navbar from './components/navbar/navbar';
+
+function App() {
+
+  return (
+    <>
+    
+      <Navbar/>
+      {/* <Login/> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+// function navbar() {
+
+
+// return (
+//     <>
+//         <div>
+//             ho
+//         </div>
+//     </>
+// )
+// }
+
+// export default navbar
+// import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      ho
+    </div>
+  )
+}
+
+export default Navbar
