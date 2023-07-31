f90a54d5db3e538d622381e1af86a9b421625856
mohssin <mohssinassalh@gmail.com>
2023-07-31 10:26:51 +0100

    second_commit_M

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
+import Home from './components/Home/Home';
 // import Login from './components/Login/Login';
-import Navbar from './components/navbar/navbar';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
   return (
     <>
-    
       <Navbar/>
-      {/* <Login/> */}
+      <Home/>
     </>
   )
 }
