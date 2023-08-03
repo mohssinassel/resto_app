46a9208485c31c2658f6997d464148285657a327
boujrada yassine <yassine.boujrada@gmail.com>
2023-08-03 18:37:39 +0100

    adding routes

--- backEnd/routes/dishRouter.js
+++ backEnd/routes/dishRouter.js
@@ -2,11 +2,12 @@ const dishesController = require("../controllers/dishesController");
   
 const router = require("express").Router();
 
-// router.post("/login", login);
-// router.post("/register", register);
-// router.get("/allusers/:id", getAllUsers);
-// router.post("/setavatar/:id", setAvatar);
-// router.get("/logout/:id", logOut);
+router.post("/addDish", dishesController.addDish);
+router.get("/getAll", dishesController.getAllDishes);
+router.get("/getDish/:dishename", dishesController.getDishByName);
+router.put("/updateDish/:id", dishesController.updateDish);
+router.delete("/deleteDish/:id", dishesController.deleteDish);
+router.get("/getDish/:id", dishesController.getDish);
 
 // make this file so i can use it with authRoutes(router) in index.js
 const dishRoutes = (mainrouter) => {
